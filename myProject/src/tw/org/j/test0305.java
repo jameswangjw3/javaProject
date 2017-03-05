7f5677c7c1e3045b51ea5a40fa071eadb300a75c
zhengyi <wangzhengyi82@yahoo.com.tw>
2017-03-05 15:40:33 +0800

    運算子

--- myProject/src/tw/org/j/test0305.java
+++ myProject/src/tw/org/j/test0305.java
@@ -8,8 +8,15 @@ public class test0305 {
 		// FIXME HAVE BUG
 		char var1 = 'a';  //ASCII: 97
 		char var2 = 100;  //ASCII: d
-		
+		int a = 10, b = 3;
+		System.out.println(a % b);        //取餘數
 		System.out.println(var1 + var2);  //數學運算
+		
+		//boolean
+		boolean var3 = true;    // not boolean var3 = 0;
+		boolean var4 = false;
+		
+		
 	}
 
 }
