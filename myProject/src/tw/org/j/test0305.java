47c297d8070f9ff3d68dc4145833a72d123ebc5f
zhengyi <wangzhengyi82@yahoo.com.tw>
2017-03-05 16:06:14 +0800

    產生 X ~ Y間的整數

--- myProject/src/tw/org/j/test0305.java
+++ myProject/src/tw/org/j/test0305.java
@@ -4,17 +4,30 @@ public class test0305 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+//==============================================================
 		// XXX	TEST
 		// FIXME HAVE BUG
-		char var1 = 'a';  //ASCII: 97
-		char var2 = 100;  //ASCII: d
-		int a = 10, b = 3;
-		System.out.println(a % b);        //取餘數
-		System.out.println(var1 + var2);  //數學運算
+//==============================================================
+//		char var1 = 'a';  //ASCII: 97
+//		char var2 = 100;  //ASCII: d
+//		int a = 10, b = 3;
+//		System.out.println(a % b);        //取餘數
+//		System.out.println(var1 + var2);  //數學運算
+//		
+//		//boolean
+//		boolean var3 = true;    // not boolean var3 = 0;
+//		boolean var4 = false;
+//==============================================================
+		double score = Math.random();
+		int intScore = (int)(score*101);  //產生 0 ~ 101 間的整數
+//		int intScore = (int)(score*6+1);  //產生 1 ~ 6        間的整數
+		System.out.println(intScore);
 		
-		//boolean
-		boolean var3 = true;    // not boolean var3 = 0;
-		boolean var4 = false;
+		if (intScore>=60){
+			System.out.println("Pass");
+		}else{
+			System.out.println("Down");
+		}
 		
 		
 	}
